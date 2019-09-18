7e2ed5146ec99bebec9671436080a0620082f63b
Angom Naomi <angomnd@gmail.com>
2019-09-18 23:23:56 +0300

    Modified

--- countVowels.js
+++ countVowels.js
@@ -1,15 +1,15 @@
-//<script src="countVowels.js"></script>
 function count(word){
+    let foundvowels=new Array();
     let vowels=['a','e','i','o','u','A','E','I','O','U'];
     let counting=0;
     for(let a=0; a<word.length; a++){
       if (vowels.indexOf(word[a])!==-1){
         counting+=1;
+        foundvowels.push(word[a]);
       }
     }
     return counting;
   }
-  
   console.log(count("Eloquent Javascript"));
   
   
