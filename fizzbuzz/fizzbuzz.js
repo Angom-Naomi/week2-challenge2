e1ae4e39a2a3ed0e18dc0818f6696bce96e1e742
Angom Naomi <angomnd@gmail.com>
2019-09-18 23:16:11 +0300

    Modified

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,13 +1,13 @@
-//<script src="fizzbuzz.js"></script>
 function fizzBuzz(array1,array2){
-    if (sum%3===0){
+    let sum=array1.length + array2.length;
+    if(sum%3===0 && sum%5==0 ){
+        return ("Fizzbuzz");
+    }
+    else if (sum%3==0){
         return ("Fizz");
     }
-    else if(sum%5===0){
+    else if(sum%5==0){
         return ("Buzz");
-    }
-    else if(sum%3===0 && sum%5===0){
-        return ("FizzBuzz");
     }
     else {
         return sum;
@@ -15,7 +15,6 @@ function fizzBuzz(array1,array2){
 }
 let array1=[1,2,3,4,5,6,7,8,9];
 let array2=[1,2,3,4,5];
-let sum=array1.length + array2.length;
 console.log(fizzBuzz(array1,array2));
 
 module.exports = fizzBuzz;
